9ef06014f9166b7538bf0b3406d458a3bf8bf6d8
michel <michelingabire190508@daystar.ac.ke>
2022-05-22 04:25:14 +0300

    added static roots

--- barber_appointment/settings.py
+++ barber_appointment/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 import cloudinary
 import cloudinary.uploader
@@ -158,6 +158,7 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/barber/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
